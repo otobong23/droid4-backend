c7d929ed44077edbd7688faf3015635539cc5ec2
miracle boniface <udohmiracle23@gmail.com>
2025-10-31 13:00:26 +0100

    repo updated

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://droidindex-web4.com', 'https://www.droidindex-web4.com'], // allow requests from your frontend
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://droidindex-web4.com', 'https://www.droidindex-web4.com'], // allow requests from your frontend
     credentials: true,               // allow cookies/auth headers if needed
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
